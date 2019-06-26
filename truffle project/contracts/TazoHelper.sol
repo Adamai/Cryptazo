41921239bc351ba89618d2b7098ea395dbe383ea
Rodrigo <araujobrodrigo@gmail.com>
2019-06-26 17:04:18 -0300

    uint to str

--- truffle project/contracts/TazoHelper.sol
+++ truffle project/contracts/TazoHelper.sol
@@ -36,14 +36,33 @@ contract TazoHelper is TazoFuse {
     return result;
   }
 
-  function getTazos() external view returns(uint[] memory) {
+  function _uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+    if (_i == 0) {
+        return "0";
+    }
+    uint j = _i;
+    uint len;
+    while (j != 0) {
+        len++;
+        j /= 10;
+    }
+    bytes memory bstr = new bytes(len);
+    uint k = len - 1;
+    while (_i != 0) {
+        bstr[k--] = byte(uint8(48 + _i % 10));
+        _i /= 10;
+    }
+    return string(bstr);
+  }
+
+  function getTazos() public view returns(string memory) {
     uint[] memory result = new uint[](tazos.length);
     uint counter = 0;
     for (uint i = 0; i < tazos.length; i++) {
         result[counter] = i;
-        counter++; 
+        counter++;
     }
-    return result;
+    return _uint2str(tazos.length);
   }
 
 }
--- truffle project/contracts/Test.sol
+++ truffle project/contracts/Test.sol
@@ -2,9 +2,15 @@ pragma solidity ^0.5.0;
 
 contract Test{
     string greet;
+    uint number = 10;
 
     constructor() public {
         greet = "hello";
+    }
+
+    function getNumber() public view returns(string memory){
+        string memory numero = _uint2str(number);
+        return numero;
     }
 
     function getGreet() public view returns (string memory) {
@@ -13,4 +19,24 @@ contract Test{
     function setGreet(string memory _newGreet) public {
         greet = _newGreet;
     }
+
+    function _uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+    if (_i == 0) {
+        return "0";
+    }
+    uint j = _i;
+    uint len;
+    while (j != 0) {
+        len++;
+        j /= 10;
+    }
+    bytes memory bstr = new bytes(len);
+    uint k = len - 1;
+    while (_i != 0) {
+        bstr[k--] = byte(uint8(48 + _i % 10));
+        _i /= 10;
+    }
+    return string(bstr);
+    }
+
 }
